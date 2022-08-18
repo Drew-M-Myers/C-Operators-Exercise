33604e676fa8f3851be78abf799b011350b1d46c
Drew Myers <drew.myers59@gmail.com>
2022-08-17 23:35:39 -0400

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OperatorExcersice
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4) ;
+
+            Console.WriteLine($"The answer of {a}/{b} is {quotient} with a remainder of {remainder}");
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            double AreaOfCircle = (Math.PI * radius * radius);
+            Console.WriteLine($"The area of a circle with {radius} is {AreaOfCircle}");
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
